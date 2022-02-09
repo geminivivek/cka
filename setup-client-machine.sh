e1588f90f6f5ec8e80e662b16e4733c60276210b
devops <devops@control-01.example.local>
2022-02-09 16:35:21 +0000

    delete

--- setup-client-machine.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install -y vim bash-completion jq 
-source <(kubectl completion bash)
-alias k=kubectl
-complete -F __start_kubectl k
-echo "autocmd FileType yaml setlocal ai ts=2 sw=2 et" >> ~/.vimrc
