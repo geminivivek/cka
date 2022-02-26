337c09774af44c23bbd060645a1bb2d9a924f35f
vivek gemini <vivek_gemini@yahoo.com>
2022-02-26 15:37:21 +0400

    Fixed the user setup file

--- vagrant/ubuntu/setup-user-enviornment.sh
+++ vagrant/ubuntu/setup-user-enviornment.sh
@@ -1,7 +1,6 @@
-sudo su - vagrant
-kubectl completion bash >> ~/.bashrc
-echo "alias k=kubectl" >> ~/.bashrc
-complete -F __start_kubectl k
-echo "autocmd FileType yaml setlocal ai ts=2 sw=2 sts=2 et" > ~/.vimrc
+kubectl completion bash >> /etc/bash.bashrc
+echo "alias k=kubectl" >> /etc/bash.bashrc
+echo "complete -F __start_kubectl k" >> /etc/bash.bashrc
+echo "autocmd FileType yaml setlocal ai ts=2 sw=2 sts=2 et" > /etc/vim/vimrc
 exit
 
